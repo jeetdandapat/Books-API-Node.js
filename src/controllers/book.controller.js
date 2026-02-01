429e9823c3d150eab666a8f15e8f4a820b3dbdfc
jeetdandapat <dandapatjeet3@gmail.com>
2026-02-02 01:11:39 +0530

    Initial commit: Complete Book API

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -5,12 +5,28 @@ import {
 
 /* CREATE BOOK */
 export const createBook = async (req, res) => {
-  const book = await createBookService(req.body);
-  res.status(201).json(book);
+  try {
+    const book = await createBookService(req.body);
+
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to create book",
+      error: error.message
+    });
+  }
 };
 
 /* EXPLORE BOOKS */
 export const exploreBooks = async (req, res) => {
-  const books = await exploreBooksService(req.query);
-  res.json(books);
+  try {
+    const books = await exploreBooksService(req.query);
+
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch books",
+      error: error.message
+    });
+  }
 };
